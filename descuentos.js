92c76e0a0eb0764d873728fd7baeba36db931b4f
1088330813 <78113562+1088330813@users.noreply.github.com>
2023-02-10 17:16:28 -0500

    Update descuentos.js

--- descuentos.js
+++ descuentos.js
@@ -79,29 +79,13 @@ cambiarTipoFechas(){
         this.calculationRateStrike(this.rateSpot, this.devaluation, this.numDiasPositivo );
         this.agregarNuevaInfo(this.fechaInicialMostrar,this.fechaFinalMostrar,this.numDiasPositivo, this.devaluation+"%", this.rateSpot, this.resultSDecimal)
       } else {
-      this.diaInicial = new Date(this.dateInicial).getUTCDate();
-      this.mesInicial = new Date(this.dateInicial).getUTCMonth();
-      this.anyoInicial = new Date(this.dateInicial).getUTCFullYear();
-        this.diaFinal = new Date(this.dateFinal).getUTCDate();
-        this.mesFinal = new Date(this.dateFinal).getUTCMonth();
-        this.anyoFinal = new Date(this.dateFinal).getUTCFullYear();
-
-          this.fechaInicialMostrar = new Date(this.anyoInicial,this.mesInicial,this.diaInicial).toLocaleDateString('es-CO', {
-            day: '2-digit',
-            month: '2-digit',
-            year: '2-digit',});
-            this.fechaFinalMostrar = new Date(this.anyoFinal,this.mesFinal,this.diaFinal).toLocaleDateString('es-CO', {
-              day: '2-digit',
-              month: '2-digit',
-              year: '2-digit',});
         this.numDiasNegativo = (this.totalRestasFechas * -1) / (1000 * 3600 * 24);
         this.calculationRateStrike(this.rateSpot, this.devaluation, this.numDiasNegativo );
         this.agregarNuevaInfo(this.fechaInicialMostrar,this.fechaFinalMostrar,this.numDiasNegativo, this.devaluation+"%", this.rateSpot, this.resultSDecimal)
-
       }
     },
 agregarNuevaInfo(fechai,fechaf,dias, deva, tasaSp, tasaSt) {
-const dataObject = {
+ const dataObject = {
   iFechaInicial:fechai,
   iFechaFinal:fechaf,
   iDias:dias,
@@ -111,7 +95,9 @@ const dataObject = {
   iMonto: 0,
   iBanco:"",
   }
+  console.log(dataObject)
     this.informacion.push(dataObject);
+    console.log(this.informacion)
     },
     localStorage.setItem('calculos-data',JSON.stringify(this.informacion));
     },
